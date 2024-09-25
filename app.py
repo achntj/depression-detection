e38168cd59fcb2cefe2a0b1b46025cf169fa3564
achntj <achntj@gmail.com>
2024-09-24 18:11:30 -0700

    feat: use one model and easy processing

--- app.py
+++ app.py
@@ -3,61 +3,32 @@ import streamlit as st
 import pandas as pd
 from preprocessing_universal import *
 
-with open('model_d2_normal_logreg.pkl', 'rb') as f:
-    model = pickle.load(f)
-
 with open('model_d1_normal_logreg.pkl', 'rb') as f:
-    model2 = pickle.load(f)
-
-
-with open('vectorizer_d2', 'rb') as f2:
-    vect = pickle.load(f2)
-
+    model = pickle.load(f)
 with open('vectorizer_d1', 'rb') as f2:
-    vect2 = pickle.load(f2)
+    vect = pickle.load(f2)
 
 st.header("Sentimate - A Machine Learning based depression Detection Tool")
 st.write("This tool uses Machine Learning and Natural Language Processing to predict if a person is depressed based on how their feeling. I plan to release a better model which is based on transformers soon.")
 
 input_text = st.text_input("How are you doing today?", "I am very happy today!")
+st.subheader("Prediction results: ")
 user_df = pd.DataFrame(columns=["text"], data=[[input_text]])
 user_df = preprocessing(user_df)
 X_user = user_df.tweet
 X_user_dtm = vect.transform(X_user.values.astype('U'))
 y_user_class = model.predict(X_user_dtm)
+for i in y_user_class:
+    if i==1:
+        st.write("\nResult- Most likely depressed.")
+        y_user_prob = model.predict_proba(X_user_dtm)[:, 1]
+    elif i==0:
+        st.write("\nResult- Most likely not depressed.")
+        y_user_prob = model.predict_proba(X_user_dtm)[:, 0]
+    else:
+        st.write("unexpected error")
+        
+st.write(f"Probability: {y_user_prob[0]*100:.2f}%")
 
-X_user2 = user_df.tweet
-X_user_dtm2 = vect2.transform(X_user2.values.astype('U'))
-y_user_class2 = model2.predict(X_user_dtm2)
-
-if len(y_user_class) == len(y_user_class2):
-    for i in range(len(y_user_class)):
-        if y_user_class[i] == 1 and y_user_class2[i] == 1:
-            res = "Most likely depressed."
-            y_user_prob = max(model.predict_proba(X_user_dtm)[
-                              :, 1], model2.predict_proba(X_user_dtm2)[:, 1])
-
-        elif y_user_class[i] == 1 and y_user_class2[i] == 0:
-            res = "Most likely depressed."
-            y_user_prob = model.predict_proba(X_user_dtm)[:, 1]
-
-        elif y_user_class2[i] == 1 and y_user_class[i] == 0:
-            res = "Most likely depressed."
-            y_user_prob = model2.predict_proba(X_user_dtm2)[:, 1]
-
-        elif y_user_class[i] == 0 and y_user_class2[i] == 0:
-            res = "Most likely not depressed."
-            y_user_prob = max(model.predict_proba(X_user_dtm)[
-                              :, 1], model2.predict_proba(X_user_dtm2)[:, 1])
-            
-else:
-    res = "unexpected error"
-    y_user_prob = "unexpected error"
-
-result = res
-probability = y_user_prob
-
-st.subheader("Prediction results: ")
-st.write(result)
 st.divider()
 st.caption("Made by [Achintya Jha](https://achintyajha.com)")
--- preprocessing_universal.py
+++ preprocessing_universal.py
@@ -1,26 +1,22 @@
-import numpy as np
-import pandas as pd
 import re
-import nltk
-import spacy
-import string
+import pandas as pd
+from emot.emo_unicode import UNICODE_EMOJI as UNICODE_EMO # For emojis
+from emot.emo_unicode import EMOTICONS_EMO as EMOTICONS # For EMOTICONS
+import contractions
+import pickle
 
 def text_lower(text):
     text = text.lower()
     text = text.replace('\n', ' ')
-    text = text.replace('&amp;','and')
+    text = text.replace('&amp;', 'and')
     return text
-    
-# df['text_lower'] = df['text'].apply(text_lower)
+
 
 def remove_usernames(text):
-    user_pattern = re.compile(r'(?<=^|(?<=[^a-zA-Z0-9-\.]))@([A-Za-z_]+[A-Za-z0-9_]+)')
+    user_pattern = re.compile(
+        r'(?<=^|(?<=[^a-zA-Z0-9-\.]))@([A-Za-z_]+[A-Za-z0-9_]+)')
     return user_pattern.sub(r'USER', text)
 
-# df['text_no_user'] = df['text_lower'].apply(remove_usernames)
-
-from emot.emo_unicode import UNICODE_EMOJI as UNICODE_EMO # For emojis
-from emot.emo_unicode import EMOTICONS_EMO as EMOTICONS # For EMOTICONS
 
 # Function for converting emojis into word
 def convert_emojis(text):
@@ -36,88 +32,27 @@ def convert_emoticons(text):
     
     return text
 
-# df['text_no_emoji'] = df['text_no_user'].apply(convert_emoticons)
-# df['text_no_emoji'] = df['text_no_user'].apply(convert_emojis)
-
-# Function for url s
 def remove_urls(text):
     url_pattern = re.compile(r'https?://\S+|www\.\S+')
     return url_pattern.sub(r'URL', text)
 
-#Passing the function to 'text_rare'
-# df['text_no_url'] = df['text_no_emoji'].apply(remove_urls)
-
-import contractions
 
 def word_expand(text):
-    expanded_words = []       
+    expanded_words = []
     for word in text.split():
         expanded_words.append(contractions.fix(word))
     expanded_text = ' '.join(expanded_words)
     return expanded_text
-    
-# df['text_expanded'] = df['text_no_url'].apply(word_expand)
-
-from nltk.corpus import wordnet
-from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
-wordnet_map = {"N":wordnet.NOUN, "V":wordnet.VERB, "J":wordnet.ADJ, "R":wordnet.ADV} # Pos tag, used Noun, Verb, Adjective and Adverb
-# Function for lemmatization using POS tag
-def lemmatize_words(text):
-    pos_tagged_text = nltk.pos_tag(text.split())
-    return " ".join([lemmatizer.lemmatize(word, wordnet_map.get(pos[0], wordnet.NOUN)) for word, pos in pos_tagged_text])
-
-# df["text_lemma"] = df["text_expanded"].apply(lemmatize_words)
 
 
 def remove_hashtags(text):
     no_hash = re.compile("#(\w+)")
     return no_hash.sub(r' ', text)
 
-# df['text_no_hash'] = df['text_lemma'].apply(remove_hashtags)
 
-#Creating function for tokenization
 def tokenization(text):
     text = re.split('\W+', text)
     return text
-
-# df['text_tokenized'] = df['text_no_hash'].apply(lambda x: tokenization(x.lower()))
-
-from nltk.tokenize.treebank import TreebankWordDetokenizer
-
-# df['processed_data'] = [TreebankWordDetokenizer().detokenize(text) for text in df['text_tokenized']]
-
-from textblob import TextBlob
-sentiment = []
-
-# for sentence in df['processed_data']: 
-#     sentiment.append(TextBlob(sentence).sentiment.polarity*10)
-    
-def textblob_sentiment(text):
-    return TextBlob(text).sentiment.polarity*10
-    
-# df['sentiment_processed'] = df['processed_data'].apply(textblob_sentiment)
-
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-
-sentiment_vader = SentimentIntensityAnalyzer()
-
-def vader_sentiment(text):
-    return sentiment_vader.polarity_scores(text)['compound']*10
-
-
-# df['new_vader_sentiment'] = df['processed_data'].apply(vader_sentiment)
-
-def sent_to_text(sentiment, sentiment2):
-    if sentiment < 0 and sentiment2 < 0:
-        sentiment = 'neg'
-    elif sentiment >= 0 or sentiment2 >= 0:
-        sentiment = 'pos'
-
-    return sentiment
-
-# # df['label'] = df[['sentiment_processed', 'new_vader_sentiment']].apply(sent_to_text)
-# df['label'] = df.apply(lambda x: sent_to_text(x.sentiment_processed, x.new_vader_sentiment), axis=1)
 
 
 def preprocessing(df):
@@ -125,19 +60,15 @@ def preprocessing(df):
     df['text_no_user'] = df['text_lower'].apply(remove_usernames)
     df['text_no_emoji'] = df['text_no_user'].apply(convert_emoticons)
     df['text_no_emoji'] = df['text_no_user'].apply(convert_emojis)
-    df['text_no_emoji'] = df['text_no_emoji'].apply(lambda x: x.replace('_', ' '))
+    df['text_no_emoji'] = df['text_no_emoji'].apply(
+        lambda x: x.replace('_', ' '))
     df['text_no_url'] = df['text_no_emoji'].apply(remove_urls)
     df['text_no_url'] = df['text_no_emoji'].apply(remove_urls)
     df['text_expanded'] = df['text_no_url'].apply(word_expand)
-    df["text_lemma"] = df["text_expanded"].apply(lemmatize_words)
-    df['text_no_hash'] = df['text_lemma'].apply(remove_hashtags)
-    df['text_tokenized'] = df['text_no_hash'].apply(lambda x: tokenization(x.lower()))
-    df['processed_data'] = [TreebankWordDetokenizer().detokenize(text) for text in df['text_tokenized']]
-    df['sentiment_processed'] = df['processed_data'].apply(textblob_sentiment)
-    df['new_vader_sentiment'] = df['processed_data'].apply(vader_sentiment)
-    df['label'] = df.apply(lambda x: sent_to_text(x.sentiment_processed, x.new_vader_sentiment), axis=1)
-    df = df.drop(['text', 'text_lower', 'text_no_user', 'text_no_url', 'text_no_emoji', 'text_expanded', 'text_lemma', 'text_no_hash', 'text_tokenized', 'sentiment_processed', 'new_vader_sentiment'], axis=1)
-    df.rename(columns = {'processed_data':'tweet'}, inplace = True)
-    
-    return df
+    df['text_no_hash'] = df['text_expanded'].apply(remove_hashtags)
+    df['processed_data'] = df['text_no_hash']
+    df = df.drop(['text', 'text_lower', 'text_no_user', 'text_no_url', 'text_no_emoji', 'text_expanded',
+                 'text_no_hash'], axis=1)
+    df.rename(columns={'processed_data': 'tweet'}, inplace=True)
 
+    return df
